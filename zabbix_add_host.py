dfbcd2361d83297730e73527b276735b2e15e19a
dinghao2020 <18001326539@163.com>
2017-06-13 15:00:26 +0800

    add file

---
+++ zabbix_add_host.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python2.7
+#coding=utf-8
+import json
+import urllib2
+#xiaorui.cc
+url = "http://192.168.1.127/zabbix/api_jsonrpc.php"
+header = {"Content-Type": "application/json"}
+# request json
+data = json.dumps(
+{
+    "jsonrpc":"2.0",
+    "method":"host.create",
+    "params":{
+        "host": "192.168.1.167","interfaces":
+        [{"type": 1,"main": 1,"useip": 1,"ip": "192.168.1.167","dns": "","port": "10050"}],
+        "groups": [{"groupid": "2"}],"templates": [{"templateid": "10087"}]
+        },
+    "auth":"b3555ca9177745d9e505e2fddb572ff2",
+    "id":1,
+}
+)
+# create request object
+request = urllib2.Request(url,data)
+for key in header:
+    request.add_header(key,header[key])
+# get host list
+try:
+    result = urllib2.urlopen(request)
+except URLError as e:
+    if hasattr(e, 'reason'):
+        print 'We failed to reach a server.'
+        print 'Reason: ', e.reason
+    elif hasattr(e, 'code'):
+        print 'The server could not fulfill the request.'
+        print 'Error code: ', e.code
+else:
+    response = json.loads(result.read())
+    result.close()
+    print 'ok'
